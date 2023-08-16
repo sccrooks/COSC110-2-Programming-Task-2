ba0006322a37d8c7a1a8afca3458e4848da360e8
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-08-17 03:38:25 +1000

    Added happy customer ratio to route dict

--- routes.py
+++ routes.py
@@ -9,6 +9,11 @@ def sort_route_data(list: list[dict]) -> None:
 
 
 def verify_route(string: str) -> bool:
+    """
+
+    :param string: route
+    :return:
+    """
     regex = "^\d+,\d+,\d+$"
     if re.search(regex, string) is not None:
         return True
@@ -16,17 +21,26 @@ def verify_route(string: str) -> bool:
         return False
 
 
-def read_route_data(file: str):
+def read_route_data(file: str) -> None:
+    """
+    Attempts to read requested csv file, and if successful creates a list
+    of dictionaries containing the route data
+
+    :param file: File to read data from.
+    """
     with open(file, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=' ')
         for row in reader:
-            if verify_route(', '.join(row)):
-                data = row[0].split(',')
+            route_txt = row[0]
+
+            if verify_route(route_txt):
+                data = route_txt.split(',')
 
                 route_dict = {
                     "route_id": int(data[0]),
                     "happy_passengers": int(data[1]),
-                    "unhappy_passengers": int(data[2])
+                    "unhappy_passengers": int(data[2]),
+                    "happy_customer_ratio": round(float(data[1]) / float(data[2]), 2)
                 }
                 routes.append(route_dict)
             else:
