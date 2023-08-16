552b12dd6331c490c094c5467aaef24ec2c04729
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-08-17 03:28:27 +1000

    Successfully reading csv into list of dicts

--- routes.py
+++ routes.py
@@ -18,12 +18,20 @@ def verify_route(string: str) -> bool:
 
 def read_route_data(file: str):
     with open(file, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        reader = csv.reader(csvfile, delimiter=' ')
         for row in reader:
             if verify_route(', '.join(row)):
-                print(', '.join(row))
+                data = row[0].split(',')
+
+                route_dict = {
+                    "route_id": int(data[0]),
+                    "happy_passengers": int(data[1]),
+                    "unhappy_passengers": int(data[2])
+                }
+                routes.append(route_dict)
             else:
-                print("false")
+                print("Error reading line")
 
 
 read_route_data("routes.txt")
+print(routes)
